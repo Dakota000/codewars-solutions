18a4e8fb42c1655f32109bd6681e7ab3bbde71be
Dakota Sean <dakotasean378@gmail.com>
2024-05-22 00:24:51 -0700

    added ASCIItoChars kata into 8 kyu as js

---
+++ 8 kyu/proper format/ASCIItoChar.js
@@ -0,0 +1,13 @@
+/*
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+65 --> 'A'
+97 --> 'a'
+48 --> '0
+*/
+
+//Answer
+
+const getChar = c => String.fromCharCode(c);
