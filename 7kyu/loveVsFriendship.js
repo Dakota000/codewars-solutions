4adf5d73b77b2abf4e05cbb89418c197774f487a
Dakota <dakotasean378@gmail.com>
2024-06-19 22:49:21 -0700

    undoing merge commit test

---
+++ 7kyu/loveVsFriendship.js
@@ -0,0 +1,20 @@
+/*
+If　a = 1, b = 2, c = 3 ... z = 26
+
+Then l + o + v + e = 54
+
+and f + r + i + e + n + d + s + h + i + p = 108
+
+So friendship is twice as strong as love :-)
+
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+The input will always be made of only lowercase letters and will never be empty.
+*/
+
+//Answer
+
+function wordsToMarks(string){
+  let abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  return string.split('').map(letter => abc.indexOf(letter) + 1).reduce((acc, c) => acc + c,0)
+}
