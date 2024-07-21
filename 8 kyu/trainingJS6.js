6efcde3f333de56e77b6de4889640ab11095525f
Dakota <dakotasean378@gmail.com>
2024-07-20 20:45:16 -0700

    created isThisATriangle, trainingJS6

---
+++ 8 kyu/trainingJS6.js
@@ -0,0 +1,15 @@
+/*
+Coding in function trueOrFalse, function accept 1 parameters:val, try to use the conditional statement if...else, if val value is false (val==false or it can convert to false), should return a string "false", if not, return a string "true".
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+*/
+
+//Answer
+
+function trueOrFalse(val){
+  if(!val){
+    return 'false'
+  }else{
+    return 'true'
+  }
+}
