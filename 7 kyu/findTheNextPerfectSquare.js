cf76fb670b18c899fa26166d8ee9d49c7f5349da
Dakota <dakotasean378@gmail.com>
2024-08-03 23:27:03 -0700

    created 8kyu: trainingJS9 / 7kyu: findTheNextPerfectSquare

---
+++ 7 kyu/findTheNextPerfectSquare.js
@@ -0,0 +1,18 @@
+/*
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+Examples ( Input --> Output )
+
+121 --> 144
+625 --> 676
+114 --> -1  #  because 114 is not a perfect square
+*/
+
+//Answer
+
+function findNextSquare(sq){
+    let sqNum = Math.pow((Math.sqrt(sq, 2)+1), 2)
+    
+    return sqNum%1 > 0 ? -1 : sqNum
+  }
