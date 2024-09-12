cf1c7b830c7ce69a9337b3778b638366bb46f8a8
Dakota <dakotasean378@gmail.com>
2024-09-11 19:14:24 -0700

    created maximumMultiple

---
+++ 7 kyu/maximumMultiple.js
@@ -0,0 +1,39 @@
+/*
+Task: Given a Divisor and a Bound, Find the largest integer (N), Such That, N meets these requirements
+
+Conditions:
+    1. N is divisible by divisor.
+    2. N is less than or equal to bound.
+    3. N is greater than 0.
+
+Notes:
+    The parameters (divisor, bound) passed to the function are only positive values.
+    It's guaranteed that a divisor is Found.
+
+Input >> Output Examples:
+    divisor = 2, bound = 7 ==> return (6)
+Explanation:
+    (6) is divisible by (2), (6) is less than or equal to bound (7) , and (6) is > 0.
+
+    divisor = 10, bound = 50 ==> return (50)
+Explanation:
+    (50) is divisible by (10), (50) is less than or equal to bound (50), and (50) is > 0.
+
+    divisor = 37, bound = 200 ==> return (185)
+Explanation:
+    (185) is divisible by (37), (185) is less than or equal to bound (200), and (185) is > 0.
+*/
+
+//Answer
+
+function maxMultiple(divisor, bound){
+    let arr = []
+    
+    for(let i = 0; i <= bound; i++){
+      if(i % divisor === 0){
+        arr.push(i)
+      }
+    }
+  
+    return Math.max(...arr)
+  }
