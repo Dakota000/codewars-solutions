f48d620128e0bc191a334786dd234fc1ace5b7ce
Dakota <dakotasean378@gmail.com>
2024-10-13 22:37:12 -0700

    created returnToSanity.js

---
+++ 8 kyu/returnToSanity.js
@@ -0,0 +1,13 @@
+/*
+This function should return an object, but it's not doing what's intended. What's wrong?
+*/
+
+//Answer
+
+function mystery() {
+    const results =
+      {
+        sanity: 'Hello'
+      };
+    return results;
+}
