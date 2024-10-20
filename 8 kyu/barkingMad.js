902de2b5fdc56b4f647dd95dc8c6ba9df71b6176
Dakota <dakotasean378@gmail.com>
2024-10-19 22:42:12 -0700

    created barkingMad.js

---
+++ 8 kyu/barkingMad.js
@@ -0,0 +1,21 @@
+/*
+Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+
+Use method prototypes to enable all Dogs to bark.
+*/
+
+//Answer
+
+function Dog (breed) {
+  this.breed = breed;
+  
+  this.bark = function() {
+    return "Woof";
+  };
+}
+
+var snoopy = new Dog("Beagle");
+var scoobydoo = new Dog("Great Dane");
